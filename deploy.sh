a1832d3b6148f2a3eb2364a77824f2c4df896b09
Raphael Khaiat <raphael@khaiat.org>
2021-02-03 22:31:03 +0100

    update deploy script for new helios mounted path

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ cd ~/workspace/perso/bacardi55iov2
 
 sleep 5
 
-hugo -D && cp ~/workspace/perso/bacardi55iov2/public/index.xml ~/workspace/perso/bacardi55iov2/public/rss.xml && rsync -azvhP --delete ~/workspace/perso/bacardi55iov2/public/ pi@cell:/mnt/cluster-data/containers-data/bacardi55io/public
+hugo -D && cp ~/workspace/perso/bacardi55iov2/public/index.xml ~/workspace/perso/bacardi55iov2/public/rss.xml && rsync -azvhP --delete ~/workspace/perso/bacardi55iov2/public/ pi@cell:/mnt/sshfs/helios/cluster-data/containers-data/bacardi55io/public
